6ae139f5bc89c67d60c3ffcf3026652e6427752f
Jet170 <18683140+Jet170@users.noreply.github.com>
2018-10-20 07:28:07 -0500

    Add files via upload

--- gameData.py
+++ gameData.py
@@ -1,16 +1,25 @@
 import csv
+import os
+from shutil import copyfile
 #Its reading correctly but its not writing at all!!!
+
 class gameStats:
-	def __init__(self): #Use for setup if needed
+	file = open('config.csv') #Chooses which file to use
+	configRead = csv.reader(file, delimiter=',')
+	fileData = []
+	for row in configRead: #Puts stuff from the config file into an array (fileData)
+		rowData = []
+		for col in row:
+			rowData.append(col)
+		fileData.append(rowData)
+	def __init__(self):
 		pass
 	def checkCommand(self, input): #This runs every time speech is recorded
-		file = open('config.csv') #Chooses which file to use
-		configRead = csv.reader(file, delimiter=',')
-		configWrite = csv.writer(file, delimiter=',')
 		rowCounter = 0
-		for row in configRead: #For each row in the file
+		#self.fileData[1]=["Wow", "It", "Actually", "Worked", "YAY"]
+		#self.fileData[1][0]="Changed!!!"
+		for row in self.fileData: #For each row in the file
 			if(rowCounter!=0):
-				rowCounter+=1
 				if(row[0] in input):
 					parsed = str.split(input)
 					modNum = 1 #Number to modify the quantity by
@@ -20,18 +29,20 @@ class gameStats:
 						except:
 							modNum=modNum
 					if row[1] in input: #If it was said to add
-						row[4] = int(row[4]) + modNum
+						row[4] = str(int(row[4]) + modNum)
 					if row[2] in input: #If it was said to subtract
-						row[4] = int(row[4]) - modNum
+						row[4] = str(int(row[4]) - modNum)
 					if row[3] in input: #If it was said to reset
 						row[4]="0"
 					break
-	def printQuantities(self): #Prints all of the 
-		file = open('config.csv') #Chooses which file to use
-		config = csv.reader(file, delimiter=',')
-		for row in config:
+			rowCounter+=1
+	def printQuantities(self): #Prints all of the quantities
+		for row in self.fileData:
 			print(row[0] + " " + row[4])
-	
-
-
-
+	def writeData(self):	#writes data in fileData to the file
+		#configWrite =  csv.writer(file, delimiter=',')
+		file2=open("tempConfig.csv", 'w', newline='')
+		configWrite = csv.writer(file2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		configWrite.writerows(self.fileData)
+		file2.close()
+		copyfile("tempConfig.csv", "config.csv")
